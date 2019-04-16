585eb5e1102412a0d48956d818ae63d1ba8aa966
Daniel Lindberg <daniel.lindberg.flash@gmail.com>
2019-04-16 13:48:13 +0200

    temp

--- src/Paint.jsx
+++ src/Paint.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Paint = () => {
-    let grid = {};  
-        for(let i = 0; i < 16; i++){
-            grid[i+1] = {};
-            for(let j = 0; j < 16; j++){
-                grid[i+1][j+1] = "white";
-            }
+    const [va, setVa] = useState("white");
+    let grid = [];
+    for (let i = 0; i < 16; i++) {
+        grid[i + 1] = [];
+        for (let j = 0; j < 16; j++) {
+            grid[i + 1][j + 1] = va;
         }
-        console.log(grid);
-        
+    }
+
+    const board = grid.map((btn) =>
+        <div>{btn}</div>
+    );
+
     return (
         <div>
-            
+            {board}
+            <br></br>
+            <div className="colors">
+                <button className="white">o</button>
+                <button className="blue">o</button>
+                <button className="orange">o</button>
+                <button className="black">o</button>
+                <button className="red">o</button>
+                <button className="green">o</button>
+                <button className="yellow">o</button>
+                <button className="pink">o</button>
+            </div>
         </div>
     )
 }
